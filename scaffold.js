724dcee0cbc5880fafdc477eabf7f7057d00aa0b
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-03 17:00:53 +0530

    1.0.7

--- scaffold.js
+++ scaffold.js
@@ -1,16 +1,23 @@
-// scaffold.js
 const fs = require('fs-extra');
 const path = require('path');
 
-// Path to the root of the installed package
-const packageRoot = __dirname;
+// Path to the directory where this script is located (inside node_modules)
+const currentDir = __dirname;
 
-// Path to the current working directory where the package is installed
+// Path to the project root (one level above the current directory)
+const packageRoot = path.resolve(currentDir, '../..');
+
+// Path to the current working directory where the user is installing the package
 const targetDir = process.cwd();
 
-// Log paths for debugging
 console.log('Copying files from:', packageRoot);
 console.log('To:', targetDir);
+
+// Ensure that source and target are not the same
+if (packageRoot === targetDir) {
+    console.error('Source and destination must not be the same.');
+    process.exit(1);
+}
 
 // Copy all contents from the package root to the target directory
 fs.copy(packageRoot, targetDir, { overwrite: true, filter: src => !src.includes('node_modules') }, (err) => {
