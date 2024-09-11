9170bcb16fd5f8dc0e0d2326339b09f59498634a
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-11 14:10:36 +0530

    1.0.30

--- scaffold.js
+++ scaffold.js
@@ -3,7 +3,7 @@ module.exports = (config) => {
     console.log("Config:", config)
     return {
       cypress: {
-        templates: ["node_modules/specbee-cypress"],
+        templates: ["./node_modules/specbee-cypress"],
         output: "./",
       },
     }
