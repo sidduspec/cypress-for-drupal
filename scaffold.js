3f24060c398f9ac966522d00211f227a1e6e9292
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-12 14:47:13 +0530

    1.0.11

--- scaffold.js
+++ scaffold.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { Scaffold } = require('simple-scaffold');
-const { CLIENT_RENEG_LIMIT } = require('tls');
 
 module.exports = (config) => {
   return Scaffold({
@@ -10,24 +9,23 @@ module.exports = (config) => {
     filter: (file) => {
       console.log('starting the filter')
       const isUnnecessaryFile = file.path.includes('package-lock') ||
-        file.path.includes('/bitbucket-pipeline') ||
-        file.path.includes('/scaffold.js') ||
-        file.path.includes('/README.md') ||
-        file.path.includes('/node_modules');
+        file.path.includes('bitbucket-pipeline') ||
+        file.path.includes('scaffold.js') ||
+        file.path.includes('README.md') ||
+        file.path.includes('node_modules');
         console.log(`Should include: ${!isUnnecessaryFile}`);
       return !isUnnecessaryFile;
     },
-    ignore: ['node_moduels', 'bitckucket-pipelines.yml'],
-    overwrite: (file) => {
-      console.log('starting the overwrite')
-      return file.path === path.join(__dirname, './package.json');
-    },
-    transform: (file) => {
-      console.log('starting the transform')
-      if (file.path.startsWith(path.join(__dirname, './specbee-cypress/cypress'))) {
-        return file.path.replace(path.join(__dirname, './specbee-cypress/cypress/'), path.join(__dirname, '../../'));
-      }
-      return file.path;
-    },
+    // overwrite: (file) => {
+    //   console.log('starting the overwrite')
+    //   return file.path === path.join(__dirname, './package.json');
+    // },
+    // transform: (file) => {
+    //   console.log('starting the transform')
+    //   if (file.path.startsWith(path.join(__dirname, './specbee-cypress/cypress'))) {
+    //     return file.path.replace(path.join(__dirname, './specbee-cypress/cypress/'), path.join(__dirname, '../../'));
+    //   }
+    //   return file.path;
+    // },
   });
 };
