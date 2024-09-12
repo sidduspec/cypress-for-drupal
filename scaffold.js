7903756cb1579d0caf5586eb327e28a54e00c4f8
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-12 14:27:34 +0530

    1.0.10

--- scaffold.js
+++ scaffold.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { Scaffold } = require('simple-scaffold');
+const { CLIENT_RENEG_LIMIT } = require('tls');
 
 module.exports = (config) => {
   return Scaffold({
@@ -7,17 +8,22 @@ module.exports = (config) => {
     templates: ['.'],  // Ensure templates is an array, even if it's just one element
     output: path.join(__dirname, '../../'),
     filter: (file) => {
+      console.log('starting the filter')
       const isUnnecessaryFile = file.path.includes('package-lock') ||
-        file.path.includes('bitbucket-pipeline') ||
-        file.path.includes('scaffold.js') ||
-        file.path.includes('readme') ||
-        file.path.includes('node_modules');
+        file.path.includes('/bitbucket-pipeline') ||
+        file.path.includes('/scaffold.js') ||
+        file.path.includes('/README.md') ||
+        file.path.includes('/node_modules');
+        console.log(`Should include: ${!isUnnecessaryFile}`);
       return !isUnnecessaryFile;
     },
+    ignore: ['node_moduels', 'bitckucket-pipelines.yml'],
     overwrite: (file) => {
+      console.log('starting the overwrite')
       return file.path === path.join(__dirname, './package.json');
     },
     transform: (file) => {
+      console.log('starting the transform')
       if (file.path.startsWith(path.join(__dirname, './specbee-cypress/cypress'))) {
         return file.path.replace(path.join(__dirname, './specbee-cypress/cypress/'), path.join(__dirname, '../../'));
       }
