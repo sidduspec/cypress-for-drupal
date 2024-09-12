0cfbe39ff2452504bc8ce6d96682c90b66f11933
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-12 15:50:21 +0530

    1.0.13

--- scaffold.js
+++ scaffold.js
@@ -1,31 +1,14 @@
 const path = require('path');
 const { Scaffold } = require('simple-scaffold');
 
-module.exports = (config) => {
+module.exports = () => {
   return Scaffold({
     name: 'cypress-for-drupal',
-    templates: ['.'],  // Ensure templates is an array, even if it's just one element
+    templates: ['./cypress-for-drupal/cypress/', './cypress-for-drupal/config/', './cypress-for-drupal/package.json'],  // Ensure templates is an array, even if it's just one element
     output: path.join(__dirname, '../../'),
-    filter: (file) => {
-      // const isUnnecessaryFile = file.path.includes('package-lock') ||
-      //   file.path.includes('bitbucket-pipeline') ||
-      //   file.path.includes('scaffold.js') ||
-      //   file.path.includes('README.md') ||
-      //   file.path.includes('node_modules');
-      //   console.log(`Should include: ${!isUnnecessaryFile}`);
-      console.log(`Filtering file: ${file.path}`);
-      return !file.path.endsWith('.yml');
+    overwrite: (file) => {
+      console.log('starting the overwrite')
+      return file.path === path.join(__dirname, './cypress-for-drupal/package.json/');
     },
-    // overwrite: (file) => {
-    //   console.log('starting the overwrite')
-    //   return file.path === path.join(__dirname, './package.json');
-    // },
-    // transform: (file) => {
-    //   console.log('starting the transform')
-    //   if (file.path.startsWith(path.join(__dirname, './specbee-cypress/cypress'))) {
-    //     return file.path.replace(path.join(__dirname, './specbee-cypress/cypress/'), path.join(__dirname, '../../'));
-    //   }
-    //   return file.path;
-    // },
   });
 };
