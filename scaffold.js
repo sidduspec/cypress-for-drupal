dfdc86ca0a831c5727c0f0481c5d6a719e37cfdd
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-12 12:43:29 +0530

    1.0.6

--- scaffold.js
+++ scaffold.js
@@ -1,56 +1,28 @@
-/** @type {import('simple-scaffold').ScaffoldConfigFile} */
-// module.exports = (config) => {
-//     console.log("Config:", config)
-//     return {
-//       cypress: {
-//         templates: ["."],
-//         output: "../../test",
-//       }
-//     }
-//   }
-
 const { Scaffold } = require('simple-scaffold');
 
-module.exports =  (config) => {
-  const specbeeCypressFiles =  Scaffold({
-    templates: ['.'],
-    output: '../../',
-    // filter: (file) => {
-    //   const isUnnecessaryFile = file.path.includes('package-lock') || file.path.includes('bitbucket-pipeline') || file.path.includes('scaffold.js') || file.path.includes('readme') || file.path.includes('node_modules');
-    //   return !isUnnecessaryFile;
-    // },
-    // overwrite: (file) => {
-    //   // Overwrite only the 'package.json' file in the root directory
-    //   return file.path === './package.json';
-    // },
-    // transform: (file) => {
-    //   // Move files within the 'cypress' folder to the top-level output directory
-    //   if (file.path.startsWith('./specbee-cypress/cypress')) {
-    //     return file.path.replace('./specbee-cypress/cypress/', '../../');
-    //   }
-    //   return file.path;
-    // },
-  });
-
+module.exports = (config) => {
   return {
-    cypress: {
-      templates: specbeeCypressFiles,
+    default: {
+      name: 'cypress-for-drupal',
+      templates: ['.'],  // Ensure templates is an array, even if it's just one element
       output: '../../',
-    },
-    filter: (file) => {
-      const isUnnecessaryFile = file.path.includes('package-lock') || file.path.includes('bitbucket-pipeline') || file.path.includes('scaffold.js') || file.path.includes('readme') || file.path.includes('node_modules');
-      return !isUnnecessaryFile;
-    },
-    overwrite: (file) => {
-      // Overwrite only the 'package.json' file in the root directory
-      return file.path === './package.json';
-    },
-    transform: (file) => {
-      // Move files within the 'cypress' folder to the top-level output directory
-      if (file.path.startsWith('./specbee-cypress/cypress')) {
-        return file.path.replace('./specbee-cypress/cypress/', '../../');
-      }
-      return file.path;
+      filter: (file) => {
+        const isUnnecessaryFile = file.path.includes('package-lock') || 
+                                  file.path.includes('bitbucket-pipeline') || 
+                                  file.path.includes('scaffold.js') || 
+                                  file.path.includes('readme') || 
+                                  file.path.includes('node_modules');
+        return !isUnnecessaryFile;
+      },
+      overwrite: (file) => {
+        return file.path === './package.json';
+      },
+      transform: (file) => {
+        if (file.path.startsWith('./specbee-cypress/cypress')) {
+          return file.path.replace('./specbee-cypress/cypress/', '../../');
+        }
+        return file.path;
+      },
     },
   };
 };
