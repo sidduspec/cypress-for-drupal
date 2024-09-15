3406b6653f4a6470071bf0c4cc62ac89c7e6fa11
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-15 20:10:57 +0530

    1.0.24

--- scaffold.js
+++ scaffold.js
@@ -17,7 +17,7 @@ const path = require('path');
 const { Scaffold } = require('simple-scaffold');
 
 // Get the path of your installed package from node_modules
-const cypressForDrupalPath = path.dirname(require.resolve('cypress-for-drupal'));
+const cypressForDrupalPath = path.dirname(require.resolve('cypress-for-drupal/package.json'));
 
 module.exports = () => {
   return Scaffold({
@@ -25,24 +25,25 @@ module.exports = () => {
     templates: [
       path.join(cypressForDrupalPath, 'cypress'),
       path.join(cypressForDrupalPath, 'config'),
-      path.join(cypressForDrupalPath, 'package.json')
+      path.join(cypressForDrupalPath, 'cypress.config.js')
     ],  // Refer to installed package's paths inside node_modules
-    output: (file) => {
-      // Define the base path that you want to replace (e.g., node_modules path)
-      const basePath = path.resolve(__dirname, 'node_modules/cypress-for-drupal/cypress');
+    output: path.join(__dirname, '../../'),
+    // output: (file) => {
+    //   // Define the base path that you want to replace (e.g., node_modules path)
+    //   const basePath = path.resolve(__dirname, 'node_modules/cypress-for-drupal/cypress');
     
-      // If the file is inside 'node_modules/cypress-for-drupal/cypress', make it relative
-      if (file.startsWith(basePath)) {
-        console.log(basePath)
-        const relativePath = path.relative(basePath, file);  // Get the relative path
-        console.log(relativePath)
-        return path.join('../../', relativePath);  // Scaffold into the relative destination
-      }
+    //   // If the file is inside 'node_modules/cypress-for-drupal/cypress', make it relative
+    //   if (file.startsWith(basePath)) {
+    //     console.log(basePath)
+    //     const relativePath = path.relative(basePath, file);  // Get the relative path
+    //     console.log(relativePath)
+    //     return path.join('../../', relativePath);  // Scaffold into the relative destination
+    //   }
     
-      // For other files, you can return a general relative path
-      console.log(file)
-      return path.join('../../', path.relative(__dirname, file));
-    },
+    //   // For other files, you can return a general relative path
+    //   console.log(file)
+    //   return path.join('../../', path.relative(__dirname, file));
+    // },
   });
 };
 
