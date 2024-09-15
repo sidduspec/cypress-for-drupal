a36120ac7aa261f5eeefbbddac7e063fe396a68f
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-15 20:21:42 +0530

    1.0.27

--- scaffold.js
+++ scaffold.js
@@ -1,18 +1,3 @@
-// const path = require('path');
-// const { Scaffold } = require('simple-scaffold');
-
-// module.exports = () => {
-//   return Scaffold({
-//     name: 'cypress-for-drupal',
-//     templates: ['./node_modules/cypress-for-drupal/cypress', 'node_modules/cypress-for-drupal/config', 'node_modules/cypress-for-drupal/package.json'],  // Ensure templates is an array, even if it's just one element
-//     output: path.join(__dirname, '../../'),
-//     overwrite: (file) => {
-//       console.log('starting the overwrite')
-//       return file.path === path.join(__dirname, './cypress-for-drupal/package.json');
-//     },
-//   });
-// };
-
 const path = require('path');
 const { Scaffold } = require('simple-scaffold');
 
@@ -23,27 +8,35 @@ module.exports = () => {
   return Scaffold({
     name: 'cypress-for-drupal',
     templates: [
-      path.join(cypressForDrupalPath, 'cypress'),
-      path.join(cypressForDrupalPath, 'config'),
-      path.join(cypressForDrupalPath, 'cypress.config.js')
-    ],  // Refer to installed package's paths inside node_modules
-    // output: path.join(__dirname, '../../'),
+      path.join(cypressForDrupalPath, 'cypress'),          // Scaffold the entire cypress directory
+      path.join(cypressForDrupalPath, 'config'),           // Scaffold the config directory
+      path.join(cypressForDrupalPath, 'cypress.config.js') // Scaffold the config.js file
+    ],  
     output: (file) => {
-      //Define the base path that you want to replace (e.g., node_modules path)
-      const basePath = path.resolve(__dirname, 'node_modules/cypress-for-drupal/cypress.config.js');
-    
-      // If the file is inside 'node_modules/cypress-for-drupal/cypress', make it relative
-      if (file.startsWith(basePath)) {
-        console.log(basePath)
-        const relativePath = path.relative(basePath, file);  // Get the relative path
-        console.log(relativePath)
-        return path.join('../../', relativePath);  // Scaffold into the relative destination
+      // Define the base paths of the 'cypress' and 'config' directories and the config.js file
+      const cypressBasePath = path.join(cypressForDrupalPath, 'cypress');
+      const configBasePath = path.join(cypressForDrupalPath, 'config');
+      const configJsPath = path.join(cypressForDrupalPath, 'cypress.config.js');
+      
+      // Handle 'cypress/' files and folders
+      if (file.startsWith(cypressBasePath)) {
+        const relativePath = path.relative(cypressBasePath, file);  // Get the relative path from the 'cypress' directory
+        return path.join('cypress-to-drupal/cypress', relativePath);  // Scaffold into the 'cypress' folder under 'cypress-to-drupal'
       }
-    
-      // For other files, you can return a general relative path
-      console.log(file)
-      return path.join('../../', path.relative(__dirname, file));
+      
+      // Handle 'config/' files
+      if (file.startsWith(configBasePath)) {
+        const relativePath = path.relative(configBasePath, file);   // Get the relative path from the 'config' directory
+        return path.join('cypress-to-drupal/config', relativePath);   // Scaffold into the 'config' folder under 'cypress-to-drupal'
+      }
+
+      // Handle 'cypress.config.js' separately
+      if (file === configJsPath) {
+        return path.join('cypress-to-drupal', 'cypress.config.js');   // Scaffold the 'cypress.config.js' file into the 'cypress-to-drupal' root
+      }
+
+      // Fallback in case the file doesn't match any of the above paths
+      return path.join('cypress-to-drupal', path.relative(__dirname, file));
     },
   });
 };
-
