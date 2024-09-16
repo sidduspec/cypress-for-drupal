b0cc5d41cf4e326be7ec2d0c850d92c0547f3df3
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-16 10:40:10 +0530

    1.0.34

--- scaffold.js
+++ scaffold.js
@@ -1,28 +1,26 @@
 const path = require('path');
 const { Scaffold } = require('simple-scaffold');
 
-// Get the path of your installed package from node_modules
-const cypressForDrupalPath = path.dirname(require.resolve('cypress-for-drupal/package.json'));
-
 module.exports = () => {
   return Scaffold({
     name: 'cypress-for-drupal',
     templates: [
-      path.join(cypressForDrupalPath, 'cypress'),          // Scaffold the entire cypress directory
-      path.join(cypressForDrupalPath, 'config'),           // Scaffold the config directory
-      path.join(cypressForDrupalPath, 'cypress.config.js') // Scaffold the config.js file
-    ],  
-    output: (file) => {
-        // file is a string path
-        console.log(__dirname, path.join(__dirname, './cypress'))
-        if (file.startsWith(path.join(__dirname, './cypress'))) {
-          
-          return file.replace(
-            path.join(__dirname, './cypress'),
-            path.join(__dirname, '../../'),
-          )
-        }
-        return path.join(__dirname, '../../', file)
-    }
-  })
+      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress'),
+      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/e2e.js'),
+      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/commands.js'),
+      path.join(__dirname, 'node_modules/cypress-for-drupal/config'),
+      path.join(__dirname, 'node_modules/cypress-for-drupal/package.json')
+    ],
+      output: (file) => {
+        // Calculate the relative path from the project root
+       console.log(path.dirname(file));
+        const relativePath = path.relative(process.cwd(), path.dirname(file));
+  
+        // Check if the file is a directory
+        //const isDirectory = await fs.stat(file).then(stats => stats.isDirectory());
+  
+        // Include all files and directories for scaffolding
+        return relativePath;
+    },
+  });
 };
