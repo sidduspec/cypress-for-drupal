d66733298ef7161f3658b122fa8b030c6e74d46c
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-18 12:08:31 +0530

    New changes

--- scaffold.js
+++ scaffold.js
@@ -5,22 +5,39 @@ module.exports = () => {
   return Scaffold({
     name: 'cypress-for-drupal',
     templates: [
-      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress'),
-      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/e2e.js'),
-      path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/commands.js'),
-      path.join(__dirname, 'node_modules/cypress-for-drupal/config'),
-      path.join(__dirname, 'node_modules/cypress-for-drupal/package.json')
+      './node_modules/cypress-for-drupal',
+      '!scaffold.js',
+      '!node_modules',
+      '!.gitignore',
+      '!bitbucket-pipeline.yml',
+      '!git'
+
+      // path.join(__dirname, 'node_modules/cypress-for-drupal/cypress'),
+      // path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/e2e.js'),
+      // path.join(__dirname, 'node_modules/cypress-for-drupal/cypress/support/commands.js'),
+      // path.join(__dirname, 'node_modules/cypress-for-drupal/config'),
+      // path.join(__dirname, 'node_modules/cypress-for-drupal/package.json')
     ],
-      output: (file) => {
-        // Calculate the relative path from the project root
-       console.log(path.dirname(file));
-        const relativePath = path.relative(process.cwd(), path.dirname(file));
+    // subdir: true,
+      output: (__file) => {
+      //   // Calculate the relative path from the project root
+      //  console.log(path.dirname(file));
+      //   const relativePath = path.relative(process.cwd(), path.dirname(file));
+      //   // Check if the file is a directory
+      //   //const isDirectory = await fs.stat(file).then(stats => stats.isDirectory())
+      //   // Include all files and directories for scaffolding
+      //   return relativePath;
+
+      console.log(__file, path.isAbsolute(__file));
+      console.log(path.relative(__dirname, __file))
+      console.log(__dirname)
   
-        // Check if the file is a directory
-        //const isDirectory = await fs.stat(file).then(stats => stats.isDirectory());
-  
-        // Include all files and directories for scaffolding
-        return relativePath;
+      path.isAbsolute(__file);
+
+
+      return './'
+
+      
     },
   });
 };
