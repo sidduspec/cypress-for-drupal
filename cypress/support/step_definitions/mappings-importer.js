8ca758d81fec12b22bf590c038bb4b21fb16bae7
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-09-19 11:21:39 +0530

    1.2.0

--- cypress/support/step_definitions/mappings-importer.js
+++ cypress/support/step_definitions/mappings-importer.js
@@ -2,7 +2,7 @@ import * as homePage from '../../fixtures/mapping/pageObjects/homePage';
 import * as loginPage from '../../fixtures/mapping/pageObjects/loginPage';
 import * as drupalPageObjects from '../../fixtures/mapping/pageObjects/durpalPageObjects.json';
 import * as addUserPage from '../../fixtures/mapping/pageObjects/addUserPage.json';
-import * as discoverSemi from '../../fixtures/mapping/pageObjects/disciverSemi.json'
+import * as discoverSemi from '../../fixtures/mapping/pageObjects/discoverSemi.json'
 
 
 const selectors = Object.assign(
